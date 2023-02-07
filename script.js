bafbbc9b0f24181ac3c3370fec9e1ef4c6a1d189
BeanXR <69449112+BeanXR@users.noreply.github.com>
2023-02-07 16:48:04 +0800

    Initial commit of completely code

---
+++ script.js
@@ -0,0 +1,5 @@
+const button = document.querySelector('button')
+
+button.addEventListener('click',arg => alert('You have clicked the sign up button'))
+// of just add the function name without invoke/()
+// if adding onclick attribute to html element, need the invoke/()/functionName()
